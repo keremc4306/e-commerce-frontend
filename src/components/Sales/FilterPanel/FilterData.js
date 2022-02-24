ca0144896c90cdd475e65ef498d715caf3709500
keremc4306 <keremc4306@gmail.com>
2022-02-24 14:42:45 +0300

    Sales screen layout

--- src/components/Sales/FilterPanel/FilterData.js
+++ src/components/Sales/FilterPanel/FilterData.js
@@ -1,14 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import ProductService from '../../../services/ProductService';
 
-function FilterData({onBrandChange}) {
+function FilterData({ onBrandChange }) {
   const [brands, setBrands] = useState([]);
 
   useEffect(() => {
-    // ProductService.getProducts().then(
-    //   response => {
-    //     setProducts(response.data);
-    //   });
     getAllBrands();
   }, []);
 
@@ -19,19 +15,17 @@ function FilterData({onBrandChange}) {
   const handleBrandChange = (e) => {
     onBrandChange(e.target.getAttribute('brand'))
   }
-  
 
   return (
-    <div>
-      <ul className="list-group">
+    <div className = "col-md-3">
+      <ul className="d-flex p-4 list-group">
         {brands.map((value, index) =>
           <li className="list-group-item" key={index}>
-            <input className="form-check-input me-1" 
-            type="checkbox" 
-            aria-label="..." 
-            brand={value}
-            id={`brandcheck-${value}`}
-            onChange={handleBrandChange}/> {value}
+            <input className="form-check-input me-1"
+              type="checkbox"
+              brand={value}
+              id={`brandcheck-${value}`}
+              onChange={handleBrandChange} /> {value}
           </li>)}
       </ul>
     </div>
--- src/components/Sales/SalesScreen.js
+++ src/components/Sales/SalesScreen.js
@@ -1,47 +1,22 @@
 import React from 'react'
 import Products from '../../components/Sales/List/Products';
-import '../../components/Sales/SalesScreen.css';
 
 function SalesScreen() {
   return (
-    <div className="home row">
-    
-      <div className="home_panelList-wrap">
-      <div className="container-fluid">
-                <div className="row">
-                    <nav id="sidebarMenu" className="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
-                        <div className="position-sticky pt-3">
-                            <ul className="nav flex-column">
-                                <li className="nav-item">
-                                    <a className="nav-link" >
-                                        <span data-feather="file"></span>
-                                        Orders
-                                    </a>
-                                </li>
-                                <li className="nav-item">
-                                    <a className="nav-link" >
-                                        <span data-feather="users"></span>
-                                        Customers
-                                    </a>
-                                </li>
-                                <li className="nav-item">
-                                    <a className="nav-link" >
-                                        <span data-feather="bar-chart-2"></span>
-                                        Reports
-                                    </a>
-                                </li>
-                            </ul>
-                        </div>
-                    </nav>
-                </div>
-            </div>
-        
-        <div className="home_list-wrap col-md-9">
-          <Products />
-        </div>
 
+    <div className="container-fluid">
+      <div className="row">
+        <nav id="sidebarMenu" className="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
+          
+        </nav>
       </div>
+
+      <div className="col-md-7">
+        <Products />
+      </div>
+
     </div>
+
   )
 }
 
